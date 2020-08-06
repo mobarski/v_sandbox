d87081a96721995561dd4c2848ffd436a023f0a8
Maciej Obarski <mobarski@gmail.com>
2020-08-06 22:47:05 +0200

    bisect

--- bisect/bisect.v
+++ bisect/bisect.v
@@ -1,16 +1,16 @@
 module bisect
 
-// bisect - find insertion point to maintain sorted order
-//          use first matching position
-pub fn bisect<T>(a []T, val T) int {
+// first_available - find insertion point to maintain sorted order
+//                   use first available position
+pub fn first_available<T>(a []T, val T) int {
 	if a.len==0 { return 0 }
-	if val <= a[0] { return 0 }
+	if val < a[0] { return 0 }
 	mut i := a.len / 2
 	mut delta := i
-	// for iter in 0..50 {
+	//for iter in 0..50 {
 	for {
 		if delta>1 { delta >>=1 }
-		//println("val:$val  iter:$iter  i:$i  delta:$delta  len:$a.len")
+		//println("iter:$iter  i:$i  delta:$delta  len:$a.len")
 		if i+1 >= a.len { return a.len }
 		if val == a[i] { return i }
 		if val < a[i] {
@@ -26,9 +26,9 @@ pub fn bisect<T>(a []T, val T) int {
 	}
 }
 
-// bisect - find insertion point to maintain sorted order
-//          prefer positions to the left
-pub fn bisect_left<T>(a []T, val T) int {
+// left - find insertion point to maintain sorted order
+//        prefer positions to the left
+pub fn left<T>(a []T, val T) int {
 	if a.len==0 { return 0 }
 	mut i := a.len / 2
 	mut delta := i
@@ -56,9 +56,9 @@ pub fn bisect_left<T>(a []T, val T) int {
 	}
 }
 
-// bisect - find insertion point to maintain sorted order
-//          prefer positions to the right
-pub fn bisect_right<T>(a []T, val T) int {
+// right - find insertion point to maintain sorted order
+//         prefer positions to the right
+pub fn right<T>(a []T, val T) int {
 	if a.len==0 { return 0 }
 	mut i := a.len / 2
 	mut delta := i
--- bisect/bisect_test.v
+++ bisect/bisect_test.v
@@ -1,82 +1,76 @@
-// GENERICS doesn't work right now in tests - commenting out
-
-/*
-
-module bisect
+import bisect
 
 // bisect
 
 fn test_bisect_int_between() {
 	a := [2,4,6]
-	assert bisect<int>(a,0) == 0
-	assert bisect<int>(a,1) == 0
-	assert bisect<int>(a,3) == 1
-	assert bisect<int>(a,5) == 2
-	assert bisect<int>(a,7) == 3
+	assert bisect.first_available<int>(a,0) == 0
+	assert bisect.first_available<int>(a,1) == 0
+	assert bisect.first_available<int>(a,3) == 1
+	assert bisect.first_available<int>(a,5) == 2
+	assert bisect.first_available<int>(a,7) == 3
 }
 
 fn test_bisect_int_same() {
 	a := [1,1,1,1,1,1]
-	assert bisect<int>(a,1) == 3
+	assert bisect.first_available<int>(a,1) == 3
 }
 
 fn test_bisect_int_empty() {
 	a := []int{}
-	assert bisect<int>(a,0) == 0
-	assert bisect<int>(a,1) == 0
-	assert bisect<int>(a,2) == 0
+	assert bisect.first_available<int>(a,0) == 0
+	assert bisect.first_available<int>(a,1) == 0
+	assert bisect.first_available<int>(a,2) == 0
 }
 
-// bisect_left
+// bisect.left
 
 fn test_bisect_left_int_between() {
 	a := [2,4,6]
-	assert bisect_left<int>(a,0) == 0
-	assert bisect_left<int>(a,1) == 0
-	assert bisect_left<int>(a,2) == 0
-	assert bisect_left<int>(a,3) == 1
-	assert bisect_left<int>(a,4) == 1
-	assert bisect_left<int>(a,5) == 2
-	assert bisect_left<int>(a,6) == 2
-	assert bisect_left<int>(a,7) == 3
+	assert bisect.left<int>(a,0) == 0
+	assert bisect.left<int>(a,1) == 0
+	assert bisect.left<int>(a,2) == 0
+	assert bisect.left<int>(a,3) == 1
+	assert bisect.left<int>(a,4) == 1
+	assert bisect.left<int>(a,5) == 2
+	assert bisect.left<int>(a,6) == 2
+	assert bisect.left<int>(a,7) == 3
 }
 
 fn test_bisect_left_int_same() {
 	a := [1,1,1,1,1,1]
-	assert bisect_left<int>(a,1) == 0
+	assert bisect.left<int>(a,1) == 0
 }
 
 fn test_bisect_left_int_empty() {
 	a := []int{}
-	assert bisect_left<int>(a,0) == 0
-	assert bisect_left<int>(a,1) == 0
-	assert bisect_left<int>(a,2) == 0
+	assert bisect.left<int>(a,0) == 0
+	assert bisect.left<int>(a,1) == 0
+	assert bisect.left<int>(a,2) == 0
 }
 
-// bisect_right
+// bisect.right
 
 fn test_bisect_right_int_between() {
 	a := [2,4,6]
-	assert bisect_right<int>(a,0) == 0
-	assert bisect_right<int>(a,1) == 0
-	assert bisect_right<int>(a,2) == 1
-	assert bisect_right<int>(a,3) == 1
-	assert bisect_right<int>(a,4) == 2
-	assert bisect_right<int>(a,5) == 2
-	assert bisect_right<int>(a,6) == 3
-	assert bisect_right<int>(a,7) == 3
+	assert bisect.right<int>(a,0) == 0
+	assert bisect.right<int>(a,1) == 0
+	assert bisect.right<int>(a,2) == 1
+	assert bisect.right<int>(a,3) == 1
+	assert bisect.right<int>(a,4) == 2
+	assert bisect.right<int>(a,5) == 2
+	assert bisect.right<int>(a,6) == 3
+	assert bisect.right<int>(a,7) == 3
 }
 
 fn test_bisect_right_int_same() {
 	a := [1,1,1,1,1,1]
-	assert bisect_right<int>(a,1) == 6
+	assert bisect.right<int>(a,1) == 6
 }
 
 fn test_bisect_right_int_empty() {
 	a := []int{}
-	assert bisect_right<int>(a,0) == 0
-	assert bisect_right<int>(a,1) == 0
-	assert bisect_right<int>(a,2) == 0
+	assert bisect.right<int>(a,0) == 0
+	assert bisect.right<int>(a,1) == 0
+	assert bisect.right<int>(a,2) == 0
 }
-
-*/

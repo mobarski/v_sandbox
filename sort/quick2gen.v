45ad0d0cb592e70604b3adf926e559b12846430c
Maciej Obarski <mobarski@gmail.com>
2020-08-12 13:31:18 +0200

    quicksort

---
+++ sort/quick2gen.v
@@ -0,0 +1,42 @@
+import time
+import rand
+
+fn quicksort<T>(mut tab []T, left u64, right u64) {
+	mut i := left
+	mut j := right
+	mut pivot := tab[(left+right)>>1] // fixed pivot
+	//mut pivot := tab[left+rand.intn(right-left)] // random pivot
+
+	for {
+		for tab[i] < pivot { i++ }
+		for tab[j] > pivot { j-- }
+		if i<=j {
+			tab[i],tab[j] = tab[j],tab[i]
+			i++
+			j--
+		}
+		if i>j { break }
+	}
+
+	if left<j  { quicksort<T>(mut tab, left, j)  }
+	if right>i { quicksort<T>(mut tab, i, right) }
+}
+
+mut total1 := i64(0)
+mut total2 := i64(0)
+for _ in 0..100 {
+	mut a := []int{}
+	for _ in 0..100000 {
+		a << rand.intn(10000)
+	}
+	mut b := a.clone()
+	sw1 := time.new_stopwatch({})
+	quicksort<int>(mut a, 0, a.len-1)
+	total1 += sw1.elapsed().microseconds()
+	sw2 := time.new_stopwatch({})
+	b.sort()
+	total2 += sw2.elapsed().microseconds()
+	assert a==b
+}
+println(total1)
+println(total2)

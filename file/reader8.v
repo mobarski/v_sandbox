798e5ac12149f2388302906064974ae4a97857b9
Maciej Obarski <mobarski@gmail.com>
2020-08-30 23:04:38 +0200

    wip: read_line

--- file/reader8.v
+++ file/reader8.v
@@ -2,7 +2,9 @@ import time
 import os
 
 const (
-	default_buf_len = 1024
+	// value optimized for reading big files
+	default_buf_len = 128*1024
+	default_buf_max = 0
 )
 
 struct Reader {
@@ -18,10 +20,9 @@ mut:
 }
 // */
 fn new_reader(file os.File) &Reader {
-	buf_len := default_buf_len
 	r := &Reader{
 		file:      file
-		buf:       []byte{len:buf_len}
+		buf:       []byte{}
 		buf_max:   0
 		offset:    0
 		carry:     0
@@ -34,46 +35,46 @@ fn new_reader(file os.File) &Reader {
 
 fn (mut r Reader) close() {
 	r.file.close()
-	// TODO r.buf ???
+	// TODO free the buffer ?
 }
 
+// private
 fn (mut r Reader) set_buffer(buf_len int, buf_max int) {
 	assert buf_len >= 2
+	assert buf_len >= r.buf.len
 	assert buf_len <= buf_max || buf_max==0
-	// TODO 
-	//r.buf = v_realloc(r.buf, u32(buf_len))
-	//r.buf_len = buf_len
-	r.buf = []byte{len:buf_len}
+	r.buf << [byte(0)].repeat(buf_len-r.buf.len)
 	r.buf_max = buf_max
-	
 }
 
 // TODO take generic delim, not only `\n`
 // TODO \r removal
-// TODO wrap C.fread -> os.File.read_into(byteptr, cnt) ???
 
 // read_line
 [direct_array_access]
 fn (mut r Reader) read_line() ? (string,bool) {
 	unsafe {
+		
+		// read file into buffer (when necessary)
 		if r.do_read {
 			if r.is_end { return none } // it's faster when it's here vs before if r.do_read
-			r.n_read = r.file.read_bytes_into(r.buf[r.carry..], r.buf.len-r.carry) + r.carry
-			//println('BUF ${r.carry} ${r.n_read} ${r.buf}')
+			if r.buf.len == 0 { // doing it here allows call to .set_buffer to be optional
+				r.set_buffer(default_buf_len, default_buf_max)
+			} 
+			r.n_read = r.file.read_bytes_into(r.buf[r.carry..], r.buf.len-r.carry) + r.carry // TODO rename r.n_read
+			//eprintln('BUF ${r.carry} ${r.n_read} ${r.buf}')
 			r.do_read = false
 			if r.n_read == 0 { return none }
 		}
 		
-		// scan
-		start := r.offset
-		loc := r.buf[start..r.n_read].index(`\n`)
-		
-		
+		// scan buffer
+		idx := r.buf[r.offset..r.n_read].index(`\n`)
+				
 		// found
-		if loc>=0 {
-			len := loc
+		if idx>=0 {
+			len := idx
 			r.offset += len+1
-			return tos(&r.buf[start], len),false
+			return tos(&r.buf[r.offset], len),false
 		}
 		
 		len := r.n_read - r.offset
@@ -82,20 +83,20 @@ fn (mut r Reader) read_line() ? (string,bool) {
 		if r.n_read < r.buf.len-r.carry {
 			r.is_end = true
 			r.do_read = true
-			return tos(&r.buf[start], len),false // TODO check golang 
+			return tos(&r.buf[r.offset], len),false // TODO check golang 
 		}
 		
-		// not found, grow (long line)
+		// not found (and line is longer than the buffer)
 		if r.offset == 0 {
 			
-			// grow limit reached -> is_prefix
+			// buffer size limit reached
 			if r.buf_max != 0 && r.buf.len >= r.buf_max {
 				r.carry = 0
 				r.do_read = true
 				return tos(&r.buf[0], len),true
 			}
 			
-			// grow
+			// grow the buffer
 			r.carry = r.buf.len
 			mut new_size := r.buf.len*2
 			if r.buf_max != 0 {
@@ -103,11 +104,10 @@ fn (mut r Reader) read_line() ? (string,bool) {
 					new_size = r.buf_max
 				}
 			} 
-			//println('growing $new_size')
-			//r.buf_len = new_size
-			//r.buf = v_realloc(r.buf, u32(r.buf_len))
+			eprintln('growing to $new_size')
+			r.buf << [byte(0)].repeat(new_size-r.buf.len) // TODO better way?
 		
-		// not found, carry
+		// not found, carry start of the line to the buffer's head
 		} else {
 			r.carry = r.n_read - r.offset
 			for i in 0..r.carry {
@@ -130,9 +130,9 @@ sw := time.new_stopwatch({})
 //filename := "C:\\repo\\orwell_new\\sample_50v2.tsv"
 filename := "usunmnie.txt"
 
-f := os.open(filename)?
+f := os.open(filename) ?
 mut r := new_reader(f)
-r.set_buffer(100_000,100_000)
+//r.set_buffer(512*1024,0)
 mut lines := 0
 for {
 	r.read_line() or { break }
